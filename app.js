f703007ecc639930fa1b4c08f88cd369e9de4e79
darkimpow <darkimpowell@gmail.com>
2023-02-05 12:55:12 -0500

    close #1

---
+++ app.js
@@ -0,0 +1,7 @@
+//declared variables will be placed here
+
+const list = document.querySelector('todo-list');
+const itemCountSpan = document.querySelector('item-count');
+const uncheckedCountSpan = document.querySelector('unchecked-count');
+const todoInput = document.querySelector('todo-input')
+
